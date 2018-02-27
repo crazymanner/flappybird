36f044706ee80ccb0b342bc8f5ae30b0e97c4d09
crazymanner <30927042+crazymanner@users.noreply.github.com>
2018-02-27 21:59:34 +0900

    기본 움직임 완료

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var projectPath = '/flappybird/';
 // elemets
 var game = document.getElementById('game');
 var sky = document.getElementById('sky');
+var pipeContainer = document.getElementById('pipe_container');
 var flappy = document.getElementById('flappy');
 
 // offset
@@ -14,14 +15,24 @@ var flappyWidth = flappy.offsetWidth;
 var flappyHeight = flappy.offsetHeight;
 var skyWidth = sky.offsetWidth;
 var skyHeight = sky.offsetHeight;
+var pipeWidth = skyWidth/10;
+var safeHeight = skyHeight/4;
+
+////////////////////////////////////////////////////////
+// size
+////////////////////////////////////////////////////////
 
 function onResize(e) {
-    // game size update
     skyWidth = sky.offsetWidth;
     skyHeight = sky.offsetHeight;
-    // flappy size update
+    pipeWidth = skyWidth/10;
+    safeHeight = skyHeight/4; 
     setFlappySize();
 }
+
+////////////////////////////////////////////////////////
+// flappy
+////////////////////////////////////////////////////////
 
 function setFlappySize() {
     flappy.style.width = skyWidth/11 + 'px';
@@ -35,28 +46,83 @@ function setFlappyPos() {
 }
 
 function jumpFlappy() {
+    //console.log('jump');
     var top = parseInt(flappy.style.top);
-    top -= 300;
+    top -= skyHeight/4;
     if (top < 0) {
         top = 0;
     }
     flappy.style.top = top + 'px';
 }
 
+////////////////////////////////////////////////////////
+// Pipe
+////////////////////////////////////////////////////////
+function addPipe() {    
+    var pipeTotalHeight = (skyHeight - safeHeight) - (safeHeight);
+    var pipeTopHeight = (pipeTotalHeight * Math.random());
+    var pipeBotHeight = pipeTotalHeight - pipeTopHeight;
+    pipeTopHeight += safeHeight/2;
+    pipeBotHeight += safeHeight/2;
+    var pipeTop = document.createElement('div');
+    pipeTop.classList.add('pipe');
+    pipeTop.classList.add('pipe_top');
+    pipeTop.style.top = 0 + 'px';
+    pipeTop.style.left = skyWidth + 'px';
+    pipeTop.style.width = pipeWidth + 'px';
+    pipeTop.style.height = pipeTopHeight + 'px';
+    pipeContainer.appendChild(pipeTop);
+
+    var pipeBot = document.createElement('div');
+    pipeBot.classList.add('pipe');
+    pipeBot.classList.add('pipe_bot');
+    pipeBot.style.bottom = 0 + 'px';
+    pipeBot.style.left = skyWidth + 'px';
+    pipeBot.style.width = pipeWidth + 'px';
+    pipeBot.style.height = pipeBotHeight + 'px';
+    pipeContainer.appendChild(pipeBot);
+}
+
+////////////////////////////////////////////////////////
+// event
+////////////////////////////////////////////////////////
+
+function run() {
+    gravity();
+    movePipe();
+}
+
 function gravity() {
    var top = parseInt(flappy.style.top);
-   top += 50;
+   top += skyHeight/15;
    if (top > skyHeight - flappyHeight) {
        top = skyHeight - flappyHeight;
    }
    flappy.style.top = top + 'px';
 }
 
+function movePipe() {
+    var pipes = document.getElementsByClassName('pipe');
+    for (var i = 0; i < pipes.length; i++) {
+        var pipe = pipes[i];
+        var left = parseInt(pipe.style.left);
+        left -= skyWidth/30;
+        pipe.style.left = left + 'px'; 
+    }
+}
+
+////////////////////////////////////////////////////////
+// main
+////////////////////////////////////////////////////////
+
 setFlappySize();
 setFlappyPos();
 
-setInterval(gravity, 100);
+setInterval(run, 100);
+setInterval(addPipe, 1500);
 
-// event
+
+// user event
 game.ontouchstart = jumpFlappy;
 game.onmousedown = jumpFlappy;
+document.onkeydown = jumpFlappy;
